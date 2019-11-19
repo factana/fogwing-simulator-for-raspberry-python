4ed9bdb61213c7e03dc22f3db8e4bbfe6de4e563
sarvesh <sarvesh.gupta@factana.com>
2019-11-19 17:25:44 +0530

    added code for sample message

---
+++ fw-iothub-sample-message/simulated_device_message_single_run.py
@@ -0,0 +1,94 @@
+''' * Copyright (C) 2019 Factana Computing Pvt Ltd.
+    * All Rights Reserved.
+    * This file is subject to the terms and conditions defined in
+    * file 'LICENSE.txt', which is part of this source code package.'''
+
+import paho.mqtt.client as mqtt
+import json
+import time
+
+# Do not change Fogwing IoT Hub host, port and topic
+host_name = 'iothub.community.fogwing.net'
+port = 8883
+topic = 'fwcom/inbound/edge/data'
+
+# Use your Fogwing IoT Hub access credentials
+client_id = 'your_Fogwing_IoTHub_clientid'
+username = 'your_Fogwing_IoTHub_username'
+password = 'your_Fogwing_IoTHub_password'
+dev_eui = 'your_Fogwing_IoTHub_deveui'
+
+# Define the JSON message to send to Fogwing IoT Hub
+temperature = 25
+humidity = 65
+moisture = 430
+data = {
+        "client_id": client_id, "dev_eui": dev_eui,
+        "payload": {"Temperature": temperature, "Humidity": humidity, "Moisture": moisture}
+}
+
+
+def on_disconnect(client, userdata, rc):
+    if rc == 0:
+       print("Fogwing IoT Hub: Client disconnected")
+       client.connected_flag = False
+
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        client.connected_flag = True
+        print("Fogwing IoT Hub: Successfully connected")
+        print("Fogwing IoT Hub: Successfully message published ")
+    elif rc == 1:
+        print("Fogwing IoT Hub: Unacceptable protocol version of Fogwing IoT Hub")
+    elif rc == 2:
+        print("Fogwing IoT Hub: Invalid client identifier")
+    elif rc == 3:
+        print("Fogwing IoT Hub: Server is unavailable to connect")
+    elif rc == 4:
+        print("Fogwing IoT Hub: Bad username or password or client_id")
+    elif rc == 5:
+        print("Fogwing IoT Hub: You are not authorised to connect")
+    elif rc == 6:
+        print("Fogwing IoT Hub: Currently server is unused")
+
+
+# The callback for when the client receives a Messageid response from the server.
+def on_publish(client, userdata, mid, qos=1):
+    print("Fogwing IoT Hub: mid value is {} and qos value is {}".format(mid, qos))
+
+
+# The callback for when the client receives a log response from the server.
+def on_log(client, userdata, level, buf):
+    print("Fogwing IoT Hub: " + buf)
+
+
+client = mqtt.Client(client_id, clean_session=True)
+client.username_pw_set(username=username, password=password)
+client.tls_set_context(context=None)
+client.on_connect = on_connect
+client.on_log = on_log
+client.on_publish = on_publish
+client.on_disconnect = on_disconnect
+
+
+# Making connection to Fogwing IoTHub with provided host_name
+def Fogwing_IoTHub_client_telemetry_run():
+    try:
+        client.connect(host_name, port=port, keepalive=60)
+    except Exception as ex:
+        print("Fogwing IoT Hub: Connection failed: ", ex)
+    except KeyboardInterrupt:
+        print("Fogwing IoT Hub: Sample stopped")
+
+    client.loop_start()
+    client.publish(topic, json.dumps(data))
+    time.sleep(1)
+    client.loop_stop()
+
+
+if __name__ == '__main__':
+    print("Fogwing IoT Hub: Started to connecting...")
+    print("Fogwing IoT Hub: Press Ctrl+C to exit")
+    Fogwing_IoTHub_client_telemetry_run()
